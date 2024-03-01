52e8bb33094e321a4c548b3e756f7f88deb541a6
Tareq <tnabi@iu.edu>
2024-02-29 21:58:37 -0500

    updated

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -125,7 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'ciphers-service-tariqnb.onrender.com']
+
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'ciphers-service-tariqnb.onrender.com', 'ciphers-service-latest-1.onrender.com']
+
+
+
 CORS_ALLOWED_ORIGINS = [
     "https://tariqnb.github.io",
     "http://localhost:3000"
